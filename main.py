4218bef97cca6a8f6ff1164ea062bfa3ad5bf642
maxim.shcherbakov <maxim.shcherbakov@gmail.com>
2021-11-14 15:16:06 +0300

    Add new main.py file

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a the main script for testing CyberPhy package.
+# Maxim Shcherbakov
+# 13/11/21
+
+
+def print_hi(name):
+    print(f'Hereby the message, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('Welcome to CyberPhy main file')
